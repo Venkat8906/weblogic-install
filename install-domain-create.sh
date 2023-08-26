d7ee6f78914e6e055932e97d5248fceeae72377f
venkat8906 <oracle@docker.oracleworld.com>
2023-08-26 05:11:07 -0700

    weblogic install

--- install-domain-create.sh
+++ install-domain-create.sh
@@ -2,6 +2,6 @@ MIDDLEWARE=/u01/app/oracle/may23/new_middleware
 tar -zxvf  $MIDDLEWARE/jdk-8u191-linux-x64.tar.gz -C $MIDDLEWARE
 mv $MIDDLEWARE/jdk1.8.0_191 $MIDDLEWARE/jdk
 export JAVA_HOME=/u01/app/oracle/may23/new_middleware/jdk
-$JAVA_HOME/bin/java -jar  $MIDDLEWARE/fmw_12.2.1.2.0_wls.jar  -silent -reponseFile $PWD/weblogic-install/response.rsp -invPtrLoc $PWD/weblogic-install/oraInst.loc
+$JAVA_HOME/bin/java -jar  $MIDDLEWARE/fmw_12.2.1.2.0_wls.jar  -silent -responseFile $PWD/weblogic-install/response.rsp -invPtrLoc $PWD/weblogic-install/oraInst.loc
 $PWD/weblogic-install/domain.py
 
